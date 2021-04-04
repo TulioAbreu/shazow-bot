5a6b4be48ddaa9d19502b90186321225f3e15be2
TulioAbreu <marco-tulioabreu1@hotmail.com>
2021-04-04 14:41:40 -0300

    🔧 Add lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,43 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "prettier",
         "plugin:@typescript-eslint/recommended"
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module"
     },
-    "plugins": [
+    plugins: [
         "@typescript-eslint"
     ],
-    "rules": {
+    rules: {
+        "array-bracket-spacing": ["error", "never"],
+        "array-callback-return": ["error", {
+            "allowImplicit": false,
+            "checkForEach": false
+        }],
+        "arrow-spacing": ["error", {"before": true, "after": true}],
+        "camelcase": ["error", {
+            "allow": ["^UNSAFE_"],
+            "properties": "never",
+            "ignoreGlobals": true
+        }],
+        "no-mixed-operators": ["error", {
+            "groups": [
+              ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+              ["&&", "||"],
+              ["in", "instanceof"]
+            ],
+            "allowSamePrecedence": true
+        }],
+        "no-trailing-spaces": "error",
+        "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
         "linebreak-style": [
             "error",
             "unix"
