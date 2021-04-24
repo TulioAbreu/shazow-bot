532661d9b7a272fd91e80ec54b07e9875e3fdbd5
TulioAbreu <marco-tulioabreu1@hotmail.com>
2021-04-24 11:44:46 -0300

    ✅ Add command service tests

---
+++ packages/server/src/services/command/index.test.ts
@@ -0,0 +1,32 @@
+import { Source } from "chat";
+import { ExecutableCommand, execute } from ".";
+
+describe("Command", () => {
+    describe("it should ignore", () => {
+        test("empty commands", async () => {
+            const command: ExecutableCommand = {
+                name: "",
+                arguments: [],
+                source: Source.Discord,
+                userID: "",
+                userName: "",
+                message: "",
+            };
+            const result = await execute(command);
+            expect(result).toBeUndefined();
+        });
+
+        test("troll commands", async () => {
+            const command: ExecutableCommand = {
+                name: "teste",
+                arguments: [],
+                source: Source.Discord,
+                message: "!teste 00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                userID: "",
+                userName: "",
+            };
+            const result = await execute(command);
+            expect(result).toBeUndefined();
+        });
+    });
+});
