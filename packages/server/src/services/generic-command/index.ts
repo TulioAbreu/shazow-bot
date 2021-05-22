76c54a5dce36d5bf0ac0971249cec2cbcfaf7345
TulioAbreu <marco-tulioabreu1@hotmail.com>
2021-05-22 10:43:58 -0300

    🐛 Fix generic command %args output

--- packages/server/src/services/generic-command/index.ts
+++ packages/server/src/services/generic-command/index.ts
@@ -1,6 +1,7 @@
 import { Action, ActionId } from "chat";
 import { ExecutableCommand } from "../command";
 import * as GenericCommandDb from "database/lib/repositories/generic-command";
+import { GenericCommand } from "database/lib/models/generic-command";
 
 export async function executeGenericCommand(
     command: ExecutableCommand
@@ -14,15 +15,24 @@ export async function executeGenericCommand(
         return;
     }
 
-    const output = command.arguments.reduce(
-        (currentOutput, argument, index) => {
-            return currentOutput.replace(`%args${index}`, argument);
-        },
-        genericCommand.output
-    );
+    const output = getGenericCommandOutput(command.arguments, genericCommand);
 
     return {
         id: ActionId.Reply,
         body: output,
     };
+}
+
+function getGenericCommandOutput(
+    commandArguments: string[],
+    genericCommand: GenericCommand
+) {
+    return commandArguments
+        .reduce(
+            (currentOutput, argument, index) => {
+                return currentOutput.replace(`%args${index}`, argument);
+            },
+            genericCommand.output
+        )
+        .replace(/%args([0-9])+/g, "");
 }
