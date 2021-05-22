d5c04638fc98a281def14845492a2e173bb49040
TulioAbreu <marco-tulioabreu1@hotmail.com>
2021-05-22 20:29:14 -0300

    📦️ Add initial "front" package

---
+++ packages/front/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="bg-black text-white">
+      Hello, World!
+    </div>
+  );
+}
