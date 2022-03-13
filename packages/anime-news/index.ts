e6b9a5a9dfe8170457e1c53ccdbd14c0903a5672
tulioabreu <33965580+TulioAbreu@users.noreply.github.com>
2022-03-13 20:25:11 -0300

    ✨ Implement anime news scrapper

---
+++ packages/anime-news/index.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { load as cheerioLoad, Cheerio, Element } from "cheerio";
+import { Maybe } from "../utils/src/result";
+
+interface CrunchyrollNews {
+    title: string;
+    href: string;
+    abstract: string;
+    postDate: string; // TODO: Parse to date??
+    imageUrl: string;
+    short: string;
+}
+
+async function scrapCrunchyrollNews() {
+    const pageHTML = await fetchCrunchyrollNewsPage();
+    // const pageHTML = readFileSync("./crunchyrollPage.html", "utf8");
+    if (!pageHTML) {
+        console.log("Could not retrieve pageHTML");
+        return;
+    }
+
+    const news: CrunchyrollNews[] = [];
+
+    const $ = cheerioLoad(pageHTML);
+    $(".news-item").each((_index, newsItemChild) => {
+        const newsItem = $(newsItemChild);
+
+        news.push({
+            title: getNewsItemTitle(newsItem),
+            href: getNewsItemHref(newsItem),
+            abstract: getNewsItemAbstract(newsItem),
+            postDate: getNewsItemPostDate(newsItem),
+            imageUrl: getNewsItemImageUrl(newsItem),
+            short: getNewsItemShort(newsItem),
+        });
+    });
+
+    return news;
+}
+
+function getNewsItemTitle(newsItem: Cheerio<Element>): string {
+    const rawTitle = newsItem.find("h2 > a").text();
+    return sanitizeText(rawTitle);
+}
+
+function getNewsItemHref(newsItem: Cheerio<Element>): string {
+    const relativeHref = newsItem.find("h2 > a").attr("href") ?? "";
+    return "https://www.crunchyroll.com" + relativeHref;
+}
+
+function getNewsItemAbstract(newsItem: Cheerio<Element>): string {
+    return sanitizeText(newsItem.find("h3").text());
+}
+
+function getNewsItemPostDate(newsItem: Cheerio<Element>): string {
+    return sanitizeText(newsItem.find(".post-date").text());
+}
+
+function getNewsItemImageUrl(newsItem: Cheerio<Element>): string {
+    return newsItem.find(".mug").attr("src") ?? "";
+}
+
+function getNewsItemShort(newsItem: Cheerio<Element>): string {
+    return sanitizeText(newsItem.find(".short").text());
+}
+
+function sanitizeText(title: string): string {
+    return title.replace(/\s+/g, " ").trim();
+}
+
+async function fetchCrunchyrollNewsPage(): Promise<Maybe<string>> {
+    const CRUNCHYROLL_NEWS_URL = "https://www.crunchyroll.com/pt-br/news";
+    try {
+        const response = await axios.get(CRUNCHYROLL_NEWS_URL);
+        console.log("status", response.status);
+        return response.data;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        }
+    }
+}
+
+scrapCrunchyrollNews();
