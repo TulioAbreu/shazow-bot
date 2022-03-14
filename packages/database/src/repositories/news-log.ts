f7d42db95cad03fdc32639660e1debb132a2ef1e
tulioabreu <33965580+TulioAbreu@users.noreply.github.com>
2022-03-13 22:16:20 -0300

    🎨 Format news log

--- packages/database/src/repositories/news-log.ts
+++ packages/database/src/repositories/news-log.ts
@@ -1,14 +1,11 @@
 import NewsLogDb, { NewsLog } from "../models/news-log";
 
 export async function getRecentNews(offset = 10): Promise<NewsLog[]> {
-    return NewsLogDb
-        .find({})
-        .sort({ sentAt: -1 })
-        .limit(offset).lean<NewsLog[]>() ?? [];
+    return NewsLogDb.find({}).sort({ sentAt: -1 }).limit(offset).lean<NewsLog[]>() ?? [];
 }
 
 export async function save(newsLog: NewsLog): Promise<NewsLog> {
     return NewsLogDb.create({
-        ...newsLog
+        ...newsLog,
     });
 }
