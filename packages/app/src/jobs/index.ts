1ee7551da49c2d5d97a320a75b1481ff6e8408c0
tulioabreu <33965580+TulioAbreu@users.noreply.github.com>
2022-03-13 23:09:52 -0300

    🚑️ Change job to run every hour

--- packages/app/src/jobs/index.ts
+++ packages/app/src/jobs/index.ts
@@ -3,7 +3,7 @@ import { schedule, ScheduledTask } from "node-cron";
 import { animeNewsJob } from "./anime-news";
 
 export function registerJobs(chat: Chat): ScheduledTask[] {
-    const animeNewsScheduledJob = schedule("* * * * *", () => {
+    const animeNewsScheduledJob = schedule("0 * * * *", () => {
         animeNewsJob(chat);
     });
     return [animeNewsScheduledJob];
