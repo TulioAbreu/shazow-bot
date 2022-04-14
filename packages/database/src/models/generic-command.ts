7fc15a060ff5e7e41abdcd8a7d06655a1d023fa6
tulioabreu <33965580+TulioAbreu@users.noreply.github.com>
2022-04-14 17:55:51 -0300

    ✨ Add server ID fields to Generic Command model

--- packages/database/src/models/generic-command.ts
+++ packages/database/src/models/generic-command.ts
@@ -1,10 +1,13 @@
 import { Document, Schema, model } from "mongoose";
+import { Source } from "../types";
 
 export interface GenericCommand {
     name: string;
     output: string;
     createdAt: Date;
     isCacheable: boolean;
+    source: Source;
+    serverId: string;
 }
 
 interface IGenericCommand extends GenericCommand, Document {}
@@ -22,6 +25,14 @@ const GenericCommandSchema = new Schema({
     // TODO: Create counter commands
     createdAt: {
         type: Schema.Types.Date,
+        required: true,
+    },
+    source: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    serverId: {
+        type: Schema.Types.String,
         required: true,
     },
 });
