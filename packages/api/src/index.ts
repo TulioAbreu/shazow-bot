e9a2020fc3d5030beff8003406cee788bc30c458
tulioabreu <33965580+TulioAbreu@users.noreply.github.com>
2022-04-18 19:14:28 -0300

    ✨ Use morgan middleware

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,13 +2,16 @@ import { databaseConnect } from "database";
 import express from "express";
 import { getSecret } from "utils/dist/secret";
 import routes from "./routes";
+import morgan from "morgan";
 
 const { mongodbKey } = getSecret();
 
 const app = express();
 const PORT = 3000;
 
-app.use(routes);
+app.use(morgan("dev"));
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log("INFO | Listening to port", PORT);
