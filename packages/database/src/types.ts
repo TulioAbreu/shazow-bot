14d5eb794f5c99aac656b1cf4d1c55d3ff7ddc2c
tulioabreu <33965580+TulioAbreu@users.noreply.github.com>
2022-04-18 19:13:57 -0300

    ✨ Introduce parseSource function

--- packages/database/src/types.ts
+++ packages/database/src/types.ts
@@ -1,10 +1,22 @@
+import { Maybe } from "utils";
+
 export enum Role {
     None = 0,
     Trusted = 1,
     Admin = 2,
 }
 
-export declare enum Source {
+export enum Source {
     Discord = 0,
     Twitch = 1,
+}
+
+export function parseSource(rawSourceStr: string): Maybe<Source> {
+    const sourceStr = rawSourceStr.trim().toLowerCase();
+    switch (sourceStr) {
+        case "discord":
+            return Source.Discord;
+        case "twitch":
+            return Source.Twitch;
+    }
 }
